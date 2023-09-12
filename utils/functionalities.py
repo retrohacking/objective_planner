05d58b7bc3428a57f57d6776a6d6f85c5bf76ea9
retrohacking <retro4hack@gmail.com>
2023-09-13 01:37:14 +0200

    listing functionalities

--- utils/functionalities.py
+++ utils/functionalities.py
@@ -1,7 +1,7 @@
 from . import *
 from utils.file_manager import load_json
 from utils.input_check import check_yesno
-from utils.database_manager import select_by_status, insert_plan
+from utils.database_manager import select, select_by_status, insert_plan
 
 def add_objective(db):
     configjson=load_json(CONFIG)
@@ -30,7 +30,16 @@ def dismiss_objective(db):
     pass
 
 def list_active_objectives(db):
-    pass
+    active_objectives=select_by_status(db[1], 'plans', '*', 'active')
+    for obj in active_objectives:
+        print(f"[{obj[0]}] {obj[1]} - {obj[2]}")
+    print("")
+
+def list_all_objectives(db):
+    objectives=select(db[1], 'plans', '*')
+    for obj in objectives:
+        print(f"[{obj[0]}] {obj[1]} - {obj[2]}\t[{obj[5]}]")
+    print("")
 
 def list_successful_objectives(db):
     pass
@@ -44,7 +53,8 @@ MENU_OPTIONS={"a" : ["Add a new objective", add_objective],
               "c" : ["Complete an objective", complete_objective],
               "d" : ["Dismiss an objective", dismiss_objective],
               "l" : ["List the active objectives", list_active_objectives],
-              "s" : ["List successful objectives", list_successful_objectives],
+              "la": ["List all the objectives", list_all_objectives],
+              "ls" : ["List successful objectives", list_successful_objectives],
               "q" : ["Quit\n", quit_planner]}
 
 def run_command(command,db):
